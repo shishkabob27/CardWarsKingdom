5f3149d17060470b132acef88e3bd3aad04fcb76
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2023-12-29 03:28:06 -0600

    Remove KFFUpsightVGController.cs

--- Assets/Scripts/Assembly-CSharp/UpsightRequester.cs
+++ Assets/Scripts/Assembly-CSharp/UpsightRequester.cs
@@ -3,9 +3,5 @@ public class UpsightRequester
 	public static void RequestContent(string requestId)
 	{
 		requestId = UpsightMilestoneManager.Instance.GetRequestIDWithCondition(requestId);
-		if (!string.IsNullOrEmpty(requestId))
-		{
-			Singleton<KFFUpsightVGController>.Instance.PlacementStarted(requestId);
-		}
 	}
 }
