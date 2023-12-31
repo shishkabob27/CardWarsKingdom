75c5ee93106c8bd4aeae4ee4991c970a9056b86a
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2023-12-31 04:51:55 -0600

    Target 60 FPS on android

--- Assets/Init.cs
+++ Assets/Init.cs
@@ -8,6 +8,7 @@ public class Init : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate(){
 		if (Application.isPlaying) wait++;
+		if (Application.platform == RuntimePlatform.Android) Application.targetFrameRate = 60;
 		if (wait == 60 || Application.platform != RuntimePlatform.Android) SceneManager.LoadScene("StartupScene");
 	}
 }
