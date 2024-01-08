8d110fd572192b590d33fb57169822e7a274fe49
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2024-01-07 19:26:02 -0600

    Update Init.cs

--- Assets/Init.cs
+++ Assets/Init.cs
@@ -5,13 +5,16 @@ using UnityEngine.SceneManagement;
 public class Init : MonoBehaviour {
 
 	int wait;
+
+	void Start(){
+		if (Application.platform == RuntimePlatform.Android) Application.targetFrameRate = 60;
+		CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+		CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+	}
 	
 	// Update is called once per frame
 	void FixedUpdate(){
 		if (Application.isPlaying) wait++;
-		if (Application.platform == RuntimePlatform.Android) Application.targetFrameRate = 60;
-		CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
-		CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
 		if (wait == 60 || Application.platform != RuntimePlatform.Android) SceneManager.LoadScene("BootVideoScene");
 	}
 }
